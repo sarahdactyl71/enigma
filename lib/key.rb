5bb029d9e74f301982cd68b3b9731f5db69166cb
Sarah Kirk <kirk710@gmail.com>
2017-03-26 13:18:23 -0600

    changes a few things in key file

--- lib/key.rb
+++ lib/key.rb
@@ -16,20 +16,20 @@ class Key
   
   def key_b
     new = digits.to_s.each_char.to_a
-    a = new[1] + new[2]
-    a = a.to_i
+    b = new[1] + new[2]
+    b = a.to_i
   end
 
   def key_c
     new = digits.to_s.each_char.to_a
-    a = new[2] + new[3]
-    a = a.to_i
+    c = new[2] + new[3]
+    c = a.to_i
   end
 
   def key_d
     new = digits.to_s.each_char.to_a
-    a = new[3] + new[4]
-    a = a.to_i
+    d = new[3] + new[4]
+    d = a.to_i
   end
 
 
