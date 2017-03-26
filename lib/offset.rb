c1d962d78f59b6703a067080afdc9e636d384835
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-26 14:05:58 -0600

    Commits offset file and test

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,5 +1,17 @@
+require 'date'
+require 'pry'
+
 class Offset
-  
-  def
-    
+  attr_reader :date
+  def initialize
+    @date = Date.today.strftime('%d%m%Y').to_i ** 2
+  end 
+
+  def last_four_digits
+    @date.to_s[-4..-1].to_i
+  end  
+
 end
+
+binding.pry
+""
