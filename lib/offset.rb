b418220d410805a30199f0f48f015f71579f42ea
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-27 15:03:54 -0600

    Commits offset changes and tests

--- lib/offset.rb
+++ lib/offset.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 class Offset
   attr_reader :date 
-  def initialize
-    @date = Date.today.strftime('%d%m%Y').to_i ** 2
+  def initialize(date=677665909088289) #Date.today.strftime('%d%m%Y').to_i ** 2
+        @date = date #initial tests were '260317' --> revert back to 6 dig date format
   end 
 
   def last_four_digits
