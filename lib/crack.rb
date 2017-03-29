5c013ae543f5670f2704229b499cd57b59c99f1b
Thomas Soucie <tsoucie1858@gmail.com>
2017-03-29 15:07:16 -0600

    Commits some crack changes

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,6 +1,32 @@
+requ
+
 class Crack
 
   def initialize
+    @offset = 
+     
   end
 
+  def get_secret
+    File.read("./lib/secret_message.txt")
+  end
+  
+  def char_map
+    char_map = Charactermap.new.character_map
+  end
+
+  def transformer
+    active_input = []
+    input = get_secret.chars
+    known = "..end.."
+    holder = input[-8..-1]
+    diff = []
+    until holder.empty?
+      active_input = holder.shift(4)
+      active_input.each do |char|
+        diff << (char_map["."] - char_map[char]).abs
+      end  
+    end
+    diff    
+  end  
 end
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -10,4 +10,19 @@ class CrackTest < Minitest::Test
     
     assert_instance_of Crack, c 
   end
+
+  def test_can_we_get_secret_message
+    c = Crack.new
+
+    assert_equal "2z84z.. to5lxad4o6n4s.cj1oepn90nqw0fjdo4m6 4 os4.ou4hq8hmq2", c.get_secret
+  end
+
+  def test_it_can_find_rotation
+    c = Crack.new
+
+    assert_equal 89, c.transformer
+    assert_equal 2, c.transfomer_b
+    assert_equal 1, c.transfomer_c
+    assert_equal 3, c.transfomer_d
+  end
 end
